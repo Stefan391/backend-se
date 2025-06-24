3f35996bd8b9dfd3b4d8dd2f557f2a3ec36fdccd
Stefan Cvetkovic <stefan.cvetkovic391@gmail.com>
2025-06-24 20:08:09 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend_se.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("ok");
+        }
+    }
+}
